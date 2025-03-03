67a783ff89d419fc141158696e3c3d05b0fd67e6
Mircea910 <mirceaimbuzan@yahoo.com>
2025-03-03 13:37:04 +0200

    Lab2

--- modullab1/src/main1.java
+++ modullab1/src/main1.java
@@ -1,5 +1,5 @@
 public class main1 {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         System.out.println("Cerinta lab 1 rezolvata");
         System.out.println("modificare facuta");
     }
